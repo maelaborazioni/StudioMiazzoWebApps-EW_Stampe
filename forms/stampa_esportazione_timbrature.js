875e55483957ab4a7721aa714c61879d364d6ddb
Giovanni Tizzoni <giovanni.tizzoni@gmail.com>
2018-06-25 17:24:14 +0200

    Report esportazione timbrature
    [ADD] Forms stampa_esportazione_timbrature,
    stampa_esportazione_timbrature_opzioni

---
+++ forms/stampa_esportazione_timbrature.js
@@ -0,0 +1,322 @@
+/**
+ * TODO generated, please specify type and doc for the params
+ * @param event
+ *
+ * @properties={typeid:24,uuid:"7B294BF4-9873-4701-B4E4-666BD8161A1A"}
+ */
+function stampaEsportazioneTimbrature(event)
+{
+	var params = {
+        processFunction: process_export_excel,
+        message: '', 
+        opacity: 0.2,
+        paneColor: '#434343',
+        textColor: '#EC1C24',
+        showCancelButton: false,
+        cancelButtonText: '',
+        dialogName : '',
+        fontType: 'Arial,4,25',
+        processArgs: [event]
+    };
+	
+	plugins.busy.block(params);
+	
+//	if (forms.stampa_filtri_anagrafici.validaFiltriAnagrafici())
+//	{
+//		if(validaOpzioni())
+//		{		
+//			var choice = 1;
+//			var params;
+//			
+//			switch (choice) 
+//			{
+//				case globals.FileFormats.PDF:
+//					params = {
+//						processFunction: process_export_report,
+//						message: '',
+//						opacity: 0.5,
+//						paneColor: '#434343',
+//						textColor: '#EC1C24',
+//						showCancelButton: false,
+//						cancelButtonText: '',
+//						dialogName: 'This is the dialog',
+//						fontType: 'Arial,4,25',
+//						processArgs: [event]
+//					};
+//					plugins.busy.block(params);
+//				    break;
+//				case globals.FileFormats.EXCEL:
+//					params = {
+//						processFunction: process_export_excel,
+//						message: '',
+//						opacity: 0.5,
+//						paneColor: '#434343',
+//						textColor: '#EC1C24',
+//						showCancelButton: false,
+//						cancelButtonText: '',
+//						dialogName: 'This is the dialog',
+//						fontType: 'Arial,4,25',
+//						processArgs: [event]
+//					};
+//				    plugins.busy.block(params);
+//					break;
+//	
+//				default:
+//					globals.ma_utl_showWarningDialog('Selezionare il formato di esportazione', 'i18n:hr.msg.attention');
+//			}		
+//		} 
+//		else
+//			globals.ma_utl_showWarningDialog('Controllare i valori inseriti', 'Stampa anagrafica dipendenti');
+//			
+//	}
+}
+
+/**
+ * @properties={typeid:24,uuid:"0A57F4F0-6216-4661-A570-B4867C565CA7"}
+ */
+function validaOpzioni()
+{
+	// TODO
+	return true;
+}
+
+/**
+ * TODO generated, please specify type and doc for the params
+ * @param event
+ *
+ * @properties={typeid:24,uuid:"C4DB1ECE-6EB6-4F9F-9379-D2FA71925769"}
+ */
+function process_export_report(event)
+{
+	try
+	{
+		exportReport(event);
+	}
+	catch(ex)
+	{
+		var msg = 'Metodo process_export_report : ' + ex.message;
+		globals.ma_utl_showErrorDialog(msg)
+		globals.ma_utl_logError(msg,LOGGINGLEVEL.ERROR);
+	}
+	finally
+	{
+		plugins.busy.unblock();
+	}
+}
+
+/**
+ * TODO generated, please specify type and doc for the params
+ * @param event
+ *
+ * @properties={typeid:24,uuid:"14467248-286D-4281-ADCE-F186916E7D63"}
+ */
+function process_export_excel(event)
+{
+	try
+	{
+		exportExcel(event);
+	}
+	catch(ex)
+	{
+		var msg = 'Metodo process_export_excel : ' + ex.message;
+		globals.ma_utl_showErrorDialog(msg)
+		globals.ma_utl_logError(msg,LOGGINGLEVEL.ERROR);
+	}
+	finally
+	{
+		plugins.busy.unblock();
+	}
+}
+
+/**
+ * TODO generated, please specify type and doc for the params
+ * @param event
+ *
+ * @properties={typeid:24,uuid:"7B22CE9E-A7FC-433C-943B-35C06DEE5F62"}
+ */
+function exportReport(event)
+{
+	
+}
+
+/**
+ * Lancia l'operazione lunga di esportazione delle timbrautre in excel
+ * 
+ * @param {JSEvent} event
+ *
+ * @properties={typeid:24,uuid:"98908259-3FAA-4C02-A1F6-34748A64337B"}
+ */
+function exportExcel(event)
+{
+	try
+	{
+		globals.ma_utl_setStatus(globals.Status.BROWSE,forms.stampa_filtri_anagrafici.controller.getName());
+		globals.svy_mod_closeForm(event);
+			
+		var vDate = new Date();
+		var values = 
+		{
+			op_hash		: utils.stringMD5HashBase64(idditta + vDate.toString()),
+			op_ditta	: idditta,
+			op_message	: 'Esportazione in corso...',
+			op_periodo 	: utils.dateFormat(vDate, globals.PERIODO_DATEFORMAT)
+		};
+		
+		// set parametri operazione
+		var frmOpt = forms.stampa_esportazione_timbrature_opzioni;
+		globals.startAsyncOperation
+		(
+			 createExcelFile,
+			 [idditta,frmOpt.vDallaData,frmOpt.vAllaData,frmOpt.vChkCausalizzate,frmOpt.vIdLavoratore || -1,frmOpt.vChkDatiContrattuali],
+			 null,
+			 null,
+			 globals.OpType.SET, // stampa esportazione timbrature
+			 values
+		);
+		
+		
+	}
+	catch(ex)
+	{
+		application.output(ex,LOGGINGLEVEL.ERROR);
+	}
+}
+
+/**
+ * Popola e genera il file excel con i dati richiesti
+ * 
+ * @param {Number} idDitta
+ * @param {Date} dateFrom
+ * @param {Date} dateTo
+ * @param {Boolean} causalizzate
+ * @param {Number} idLavoratore
+ * @param {Boolean} datiContrattuali
+ * 
+ * @param {JSRecord<db:/ma_log/operationlog>} operation
+ * 
+ * @properties={typeid:24,uuid:"81C7AE54-8883-4BDC-B3B8-8C94952430A8"}
+ */
+function createExcelFile(idDitta,dateFrom,dateTo,causalizzate,idLavoratore,datiContrattuali,operation)
+{
+	try
+	{
+		// LOG per inizio recupero dati
+		operation.op_message = 'Inizio estrazione dati';
+		operation.op_end = new Date();
+		operation.op_status = globals.OpStatus.ONGOING;
+		operation.op_progress = 10;
+		
+		var fileName = ['Esportazione_Timbrature'
+		                , 'Dal', utils.dateFormat(dateFrom, globals.PERIODO_DATEFORMAT)
+						, 'Al', utils.dateFormat(dateTo,globals.PERIODO_DATEFORMAT)].join('_');
+		var localFile = true;
+		/** @type {Array<byte>} */
+		var template = plugins.file.readFile('C:/Report/EW_EsportazioneTimbrature.xls');
+		
+		// array di parametri
+		/**@type Array */
+		var sqlArgs =  
+			[idDitta
+		              ,utils.dateFormat(dateFrom,globals.ISO_DATEFORMAT)
+					  ,utils.dateFormat(dateTo,globals.ISO_DATEFORMAT)
+					  ,causalizzate ? 1 : 0
+					  ,idLavoratore
+					  ,datiContrattuali ? 1 : 0];
+		        
+		// array con nomi delle colonne
+		var colNames = [];
+		// array con nomi dei tipi di colonna
+		var colTypes = [];
+		
+		// query
+		var sql = "{call S_Report_Timbrature(?,?,?,?,?,?)}";
+		
+		/** @type {Array<Number>} */
+		var typesArr = [0,0,0,0,0,0];
+		var ds = plugins.rawSQL.executeStoredProcedure(
+			                               globals.Server.MA_PRESENZE
+			                               ,sql
+			                               ,sqlArgs
+										   ,typesArr
+										   ,-1)
+		
+	    databaseManager.startTransaction();
+										   
+        if(ds == null || ds.getMaxRowIndex() == 0)
+        {
+        	operation.op_message = "Nessuna timbratura per il periodo selezionato";
+    		operation.op_end = new Date();
+    		operation.op_status = globals.OpStatus.SUCCESS;
+    		operation.op_progress = 100;
+        }
+        else
+        {
+			// array con nomi delle colonne
+			colNames = ds.getColumnNames();
+			// array con nomi dei tipi di colonna
+			colTypes = [];
+			for(var c = 0; c < colNames.length; c++)
+				colTypes.push(ds.getColumnType(c));
+			
+			var bytes = globals.xls_export(ds,fileName,localFile,false,true,null,'Esportazione timbrature',template,colNames,colTypes);
+	
+			ds.removeRow(-1);
+			
+			if(!bytes || bytes.length == 0)
+				return false;
+					
+			if(!globals.saveFile(operation, bytes, fileName + '.csv', globals.MimeTypes.CSV))
+				throw 'Errore durante il salvataggio del file';
+			
+			operation.op_message = 'Esportazione completata con successo';
+			operation.op_end = new Date();
+			operation.op_status = globals.OpStatus.SUCCESS;
+			operation.op_progress = 100;
+        }
+		
+		return true;
+	}
+	catch (ex)
+	{
+		application.output(ex, LOGGINGLEVEL.ERROR);
+		
+		if(operation)
+		{
+			operation.op_end = new Date();
+			operation.op_status = globals.OpStatus.ERROR;
+			operation.op_progress = 0;
+			operation.op_message = ex.message || 'Errore durante l\'esportazione dei dati' + (ex && (': ' + ex));
+		}
+		
+		return false;
+	}
+	finally
+	{
+		/**
+		 * Remove all created files and commit the transaction
+		 */
+		plugins.file.deleteFolder(globals.SERVER_TMPDIR.replace(/\\/g,'/') + '/export/', false);
+		databaseManager.commitTransaction();
+		
+		var retObj = {status : operation};
+		forms.mao_history.checkStatusCallback(retObj);
+		forms.mao_history.operationDone(retObj);
+	}
+}
+
+/**
+ * Callback method for when form is shown.
+ *
+ * @param {Boolean} firstShow form is shown first time after load
+ * @param {JSEvent} event the event that triggered the action
+ *
+ * @private
+ *
+ *
+ * @properties={typeid:24,uuid:"C1762B6C-C88C-4BB2-A39A-94612BB81743"}
+ */
+function onShow(firstShow, event) 
+{	
+	plugins.busy.prepare();
+	_super.onShowForm(firstShow, event);
+}
---
+++ forms/stampa_esportazione_timbrature_opzioni.js
@@ -0,0 +1,161 @@
+/**
+ * @type {Date}
+ * 
+ * @properties={typeid:35,uuid:"38501111-D3D5-4B57-965F-963A38122DF4",variableType:93}
+ */
+var vDallaData = null;
+/**
+ * @type {Date}
+ * 
+ * @properties={typeid:35,uuid:"FC04BB19-CBE7-4824-AB95-1DBFD749936E",variableType:93}
+ */
+var vAllaData = null;
+
+/**
+ * @type {Number}
+ *
+ * @properties={typeid:35,uuid:"0FBA528C-0610-411D-9E79-DF75F39740FD",variableType:4}
+ */
+var vChkSingoloDip = 0;
+
+/**
+ * @type {Number}
+ *
+ * @properties={typeid:35,uuid:"7CFF40E4-8C2B-49C4-8CAA-7BBBDE42E106",variableType:4}
+ */
+var vChkDatiContrattuali = 0;
+
+/**
+ * @type {Number}
+ *
+ * @properties={typeid:35,uuid:"DAA2DADD-6BC9-4259-BCAF-A477BBBE11D2",variableType:4}
+ */
+var vChkCausalizzate = 0;
+
+/**
+ * @type {Number}
+ * 
+ * @properties={typeid:35,uuid:"F4096BFB-844C-4185-86D1-27C23B87FB85",variableType:8}
+ */
+var vIdLavoratore = null;
+
+/**
+ * @type {Number}
+ * 
+ * @properties={typeid:35,uuid:"7A469DDE-49D0-446C-B3B4-206F343581F6",variableType:8}
+ */
+var vCodice = null;
+
+/**
+ * @type {String}
+ * 
+ * @properties={typeid:35,uuid:"3D527E23-6808-4291-8D2E-701178E40DFE"}
+ */
+var vNominativo = null;
+
+/**
+ * @properties={typeid:24,uuid:"B68D1495-07C7-43E6-B936-83D707D9039E"}
+ */
+function selectLavoratore()
+{
+	globals.ma_utl_showLkpWindow
+	(
+		{
+			  lookup: forms.stampa_esportazione_timbrature.foundset.tipologia === globals.Tipologia.ESTERNA ? 'AG_Lkp_LavoratoriEsterni' : 'AG_Lkp_Lavoratori'  
+			, returnForm: controller.getName()
+			, methodToAddFoundsetFilter: 'filterLavoratore'
+			, methodToExecuteAfterSelection : 'updateLavoratore'	
+		}
+	);
+}
+
+/**
+ * @param {JSFoundset} fs
+ *
+ * @properties={typeid:24,uuid:"5FFCC800-7EE9-41AB-BC43-57C42EB15AE8"}
+ */
+function filterLavoratore(fs)
+{
+	var frm = forms.stampa_esportazione_timbrature;
+	var arrLavInForza = globals.getLavoratoriDalAl(vDallaData,vAllaData);
+	fs.addFoundSetFilterParam('idditta','=',frm.idditta);
+	fs.addFoundSetFilterParam('idlavoratore','IN',arrLavInForza);
+	
+	return fs;
+}
+
+/**
+ * @param {JSRecord<db:/ma_anagrafiche/lavoratori>} lav
+ * 
+ * @properties={typeid:24,uuid:"8733D2E7-A06F-4D1C-8086-68A6E55D31BE"}
+ */
+function updateLavoratore(lav)
+{
+	vIdLavoratore = lav.idlavoratore;
+	vCodice = lav.codice;
+	vNominativo = lav.nominativo;
+}
+
+/**
+ * Handle changed data, return false if the value should not be accepted. In NGClient you can return also a (i18n) string, instead of false, which will be shown as a tooltip.
+ *
+ * @param {Number} oldValue old value
+ * @param {Number} newValue new value
+ * @param {JSEvent} event the event that triggered the action
+ *
+ * @return {Boolean}
+ *
+ * @properties={typeid:24,uuid:"08AD7318-C799-46F4-82B4-2647D24EB5C8"}
+ */
+function onDataChangeSingoloDipendente(oldValue, newValue, event) 
+{
+	elements.lbl_codice.enabled =
+		elements.lbl_nominativo.enabled =
+			elements.btn_lkp_dipendente.enabled = newValue;
+	elements.lbl_codice.readOnly =
+		elements.lbl_nominativo.readOnly = newValue;
+			
+	if(!newValue)
+		vIdLavoratore = vCodice = vNominativo = null;
+	
+	return true;
+}
+
+/**
+ * @properties={typeid:24,uuid:"14088335-0AB7-43DF-B556-6FF109F3D3D8"}
+ */
+function validaOptions()
+{
+	if(vDallaData == null || vAllaData == null)
+	{
+		globals.ma_utl_showWarningDialog('Controllare i valori inseriti per le date','Esportazione timbrature');
+		return false;
+	}
+	
+	if(vChkSingoloDip && vIdLavoratore == null)
+	{
+		globals.ma_utl_showWarningDialog('Si è scelto di filtrare su di un singolo dipendente ma non è stato selezionato alcun dipendente','Esportazione timbrature');
+		return false;
+	}
+	
+	return true;
+}
+/**
+ * Callback method for when form is shown.
+ *
+ * @param {Boolean} firstShow form is shown first time after load
+ * @param {JSEvent} event the event that triggered the action
+ *
+ * @private
+ *
+ * @properties={typeid:24,uuid:"C73DC669-F2F0-4CCC-AB16-D805AB1872D6"}
+ */
+function onShow(firstShow, event) 
+{
+	if(!scopes.giornaliera.haTimbratureCausalizzate(forms.stampa_esportazione_timbrature.idditta))
+	{
+		elements.chk_causalizzate.enabled = 
+			elements.lbl_causalizzate.enabled = false;
+		vChkCausalizzate = 0;	
+	}
+}
